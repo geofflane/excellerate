ca30f47915b1f05d41680bc8170dfe04bbeaa117
Geoff Lane <geoff@zorched.net>
2026-02-27 10:53:44 -0500

    Init bare project

---
+++ lib/excellerate.ex
@@ -0,0 +1,18 @@
+defmodule ExCellerate do
+  @moduledoc """
+  Documentation for `ExCellerate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCellerate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/excellerate_test.exs
@@ -0,0 +1,8 @@
+defmodule ExcellerateTest do
+  use ExUnit.Case
+  doctest Excellerate
+
+  test "greets the world" do
+    assert Excellerate.hello() == :world
+  end
+end
